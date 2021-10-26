c5e0ccca4f8497313f30abc936d1edf93df6cbf2
YuanOwO <68553129+YuanOwO@users.noreply.github.com>
2021-10-26 19:24:31 +0800

    random urls

--- assets/script/onload.js
+++ assets/script/onload.js
@@ -10,20 +10,38 @@ function now(format) {
     return format;
 }
 
+function randomLocate() {
+    location.href = './random.html';
+}
+
 var time = 0;
 function showTime() {
     time++;
     document.getElementById('time').innerHTML = now('%Y/%m/%d %H:%M:%S');
     setTimeout('showTime()', 1000);
 }
+
 function indexOnload() {
-    document.body.addEventListener('click', randomLocatoin);
+    function loadArticles(index) {
+        var xhr = new XMLHttpRequest();
+        index = Math.floor(Math.random() * index)
+        var path = './assets/articles/' + index + '.html'
+        if (index == 5) {
+            document.body.removeEventListener('click', randomLocate);
+        }
+        xhr.open('get', path);
+        xhr.send();
+        xhr.onload = function() {
+            var article = xhr.responseText;
+            document.getElementById('ariticle').innerHTML = article;
+        }
+    }
+    document.body.addEventListener('click', randomLocate);
     showTime();
-    loadUrls();
     loadArticles(10);
     setInterval(function() {
         if (time == 3) {
-            randomLocatoin();
+            randomLocate()
         }
     })
 }
